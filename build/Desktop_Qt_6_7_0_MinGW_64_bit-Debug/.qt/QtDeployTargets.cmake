f60b24b9c71348ebf0409804f2932fa342df3ff4
YuanQiiii <exqinhongwei@outlook.com>
2024-05-13 13:46:52 +0800

    添加了初始提交，包括一些文件。

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Reservoir_FILE C:/Users/exqin/Desktop/yuanqi/Reservoir/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Reservoir.exe)
+set(__QT_DEPLOY_TARGET_Reservoir_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
